863788d770d83836e72e97fee2e1ffda253c027f
venija <venijawow@gmail.com>
2024-03-08 18:18:00 +0200

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+// const passElement = document.getElementById('pass').value;
+const decodeButton = document.querySelector('.btn').onclick = function startDecode() {
+
+        while (i = passElement.value) {
+            alert( i )
+            i++
+            break
+        }
+    }
+
+
+
+let i = 0
+    passElement = document.getElementById('pass');
+
