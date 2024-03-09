6391080eff2e21117d1df9e03f44c6ed400a96a5
venija <venijawow@gmail.com>
2024-03-09 02:10:51 +0200

    переделал

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 // const passElement = document.getElementById('pass').value;
 const decodeButton = document.querySelector('.btn').onclick = function startDecode() {
 
-        // while (i < 10000) {
-        //     if( i === passElement.value)
-        //     alert( i )
-        //     i++
-        //     break
-        // }
-    let i = 0
-if (i < passElement.value) {i++}
-if (i = passElement.value) alert( i )
+        while (i < 10000) {
+        if( i == passElement.value) {
+            break;
+        } 
+        i = i + 1}
+        alert(i);
     }
-
 
 
 
 let i = 0
     passElement = document.getElementById('pass');
-
