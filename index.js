61f95ea5d9a40c061b9769efccf65962f1eb575f
venija <venijawow@gmail.com>
2024-03-10 18:07:45 +0200

    сделал через фор

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 // const passElement = document.getElementById('pass').value;
 const decodeButton = document.querySelector('.btn').onclick = function startDecode() {
 
-        while (i < 10000) {
-        if( i == passElement.value) {
-            break;
-        } 
-        i = i + 1}
-        alert(i);
+    // let i = 0
+    // while (i < 10000) {
+    //     if( i == passElement.value) {
+    //         alert(i);
+    //         break;
+    //     } 
+    //     i = i + 1;
+    // }
+        for(let i = 110; i <= passElement.value; i++) {
+            if(i < passElement.value) continue;
+            alert(i)
+
+        }  
     }
 
 
 
-let i = 0
     passElement = document.getElementById('pass');
